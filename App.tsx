6eaf18855290263972e03113978f87f3b05f8291
jitendra768 <88917543+jitendra768@users.noreply.github.com>
2023-01-17 13:56:10 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  console.log('Tjoisfds');
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-sfukam
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-sfukam)
